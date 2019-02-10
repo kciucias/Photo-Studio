d05b48c42c5f1761db8f865c3ae712ee05e9a5a7
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2019-02-10 23:12:27 +0000

    finish animations

--- js/script.js
+++ js/script.js
@@ -2,40 +2,16 @@ $(document).ready (function() {
     
     new WOW().init();
     
+    $('.js-iphone').waypoint(function(direction) {
+       
+      $('.js-iphone').addClass('animated slideInUp');   
+   }, {
+        offset: '70%'
+  });
     
-    $(".burger-icon").on("click", function(){
-        
-        $(".header nav ul").toggleClass("open");
-        
+    // hamburger nav
+    $(".burger-icon").on("click", function(){      
+        $(".header nav ul").toggleClass("open");  
     });
-    
-    
-  /*
-    
-   $('.js-wp-2').waypoint(function(direction) {
-       
-       $('.js-wp-2').addClass('animated slideInUp');
-       
-   }, {
-		offset: '70%'
-	});
-    
-    $('.js-wp-3').waypoint(function(direction) {
-       
-       $('.js-wp-3').addClass('animated fadeIn');
-       
-   }, {
-		offset: '70%'
-	});
-    
-   
-    
-    
-    
-    $(".iphone-btn").delay(2300).animate( {bottom: "+=-3"}, 300);
-    $(".iphone-btn").delay(300).animate( {top: "+=-3"}, 100);
-    
-    
- */   
     
 });
